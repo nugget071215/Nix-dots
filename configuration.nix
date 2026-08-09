fa09a7dae1a28bf38952f736f604cbd9bd576421
nugget <averybura8@gmail.com>
2026-08-09 15:05:30 -0400

    Switch desktop manager to Plasma 6 and adjust services
    Updated desktop manager to Plasma 6 and modified audio fix service.

--- configuration.nix
+++ configuration.nix
@@ -36,8 +36,7 @@
     variant = "";
   };
   services.xserver.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
-  services.desktopManager.gnome.enable = true;
+  services.desktopManager.plasma6.enable = true;
   services.printing.enable = true;
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -50,14 +49,67 @@
   services.flatpak.enable = true;
   systemd.services.fix-audio = {
     description = "Fixes audio on my laptop";
-    after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
+    after = [ "systemd-modules-load.service" ];
+    path = with pkgs; [ kmod i2c-tools ];
 
     serviceConfig = {
-      ExecStart = "/home/aves/.nix/2pa-byps.sh";
-      Restart = "on-failure";
-      User = "root";
+      Type = "oneshot";
+      RemainAfterExit = true;
     };
+
+    script = ''
+      modprobe i2c-dev
+      i2c_bus=0
+      echo "Using I2C bus: $i2c_bus"
+
+      count=0
+      for value in 0x3d 0x38; do
+          val=$((count % 2))
+          i2cset -f -y "$i2c_bus" "$value" 0x00 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x7f 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x01 0x01
+          i2cset -f -y "$i2c_bus" "$value" 0x0e 0xc4
+          i2cset -f -y "$i2c_bus" "$value" 0x0f 0x40
+          i2cset -f -y "$i2c_bus" "$value" 0x5c 0xd9
+          i2cset -f -y "$i2c_bus" "$value" 0x60 0x10
+          if [ $val -eq 0 ]; then
+              i2cset -f -y "$i2c_bus" "$value" 0x0a 0x1e
+          else
+              i2cset -f -y "$i2c_bus" "$value" 0x0a 0x2e
+          fi
+          i2cset -f -y "$i2c_bus" "$value" 0x0d 0x01
+          i2cset -f -y "$i2c_bus" "$value" 0x16 0x40
+          i2cset -f -y "$i2c_bus" "$value" 0x00 0x01
+          i2cset -f -y "$i2c_bus" "$value" 0x17 0xc8
+          i2cset -f -y "$i2c_bus" "$value" 0x00 0x04
+          i2cset -f -y "$i2c_bus" "$value" 0x30 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x31 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x32 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x33 0x01
+          i2cset -f -y "$i2c_bus" "$value" 0x00 0x08
+          i2cset -f -y "$i2c_bus" "$value" 0x18 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x19 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x1a 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x1b 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x28 0x40
+          i2cset -f -y "$i2c_bus" "$value" 0x29 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x2a 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x2b 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x00 0x0a
+          i2cset -f -y "$i2c_bus" "$value" 0x48 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x49 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x4a 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x4b 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x58 0x40
+          i2cset -f -y "$i2c_bus" "$value" 0x59 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x5a 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x5b 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x00 0x00
+          i2cset -f -y "$i2c_bus" "$value" 0x02 0x00
+          count=$((count + 1))
+      done
+    '';
   };
   users.users."aves" = {
     isNormalUser = true;
@@ -69,7 +121,7 @@
     ];
   };
   programs.firefox.enable = true;
-  services.displayManager.gdm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.upower.enable = true;
   
   hardware.bluetooth.enable = true;
@@ -78,10 +130,7 @@
   services.blueman.enable = true;
 
   nixpkgs.config.allowUnfree = true;
-  programs.hyprland.enable = true; 
-  programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
   programs.zsh.enable = true;
-  programs.niri.enable = true;
   programs.mango.enable = true;
   hardware.graphics = {
     enable = true;
@@ -90,10 +139,14 @@
   
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
     ];
+
+    config.common.default = "*";
+    config.mango.default = "gtk";
   };
 
   systemd.user.services.xdg-desktop-portal-wlr = {
@@ -224,6 +277,7 @@
     unityhub
     os-prober 
     nitch
+    ghostty
 # i like femboys
   ];
   system.stateVersion = "26.05"; 
